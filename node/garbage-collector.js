0647ec2bb749f86f7977f550b0b0055c9da01443
Lee Bock <lbock@esri.com>
2014-04-24 17:06:51 -0400

    garbage-collector.js: whole host of changes to support hiding the record rather than deleting it.

--- node/garbage-collector.js
+++ node/garbage-collector.js
@@ -2,7 +2,7 @@ var http = require("http");
 var Twit = require('twit');
 var querystring = require("querystring");
 
-if (process.argv.length < 7) {
+if (process.argv.length < 8) {
 	throw("argument required");
 }
 
@@ -14,8 +14,9 @@ var T = new Twit({
 });
 
 var FEATURE_SERVICE = process.argv[6];
+var TOKEN = process.argv[7];
 
-var QUERY_URL = FEATURE_SERVICE+"/query?where=1+%3D+1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&outFields=Tweet_ID&returnGeometry=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&f=pjson&token=";
+var QUERY_URL = FEATURE_SERVICE+"/query?where=Matched+%3D+%271%27+and+Hide+%3D+%270%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&outFields=Tweet_ID%2C+FID&returnGeometry=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&f=pjson&token=";
 
 var opts = {host: "services.arcgis.com", path: QUERY_URL};
 
@@ -34,7 +35,8 @@ function driver()
 			var features = JSON.parse(text).features;
 			for (var i = 0; i < features.length; i++)
 			{
-				check(features[i].attributes.Tweet_ID);
+				var feature = features[i];
+				check(feature.attributes.Tweet_ID, function(){hideRecord(feature.attributes.FID)});
 			}
 		});
 	});
@@ -44,59 +46,50 @@ function driver()
 }
 
 
-function check(id)
+function check(id, callBack)
 {
 	T.get('statuses/show/:id', {id:id}, function(err, reply){
 		if (err) {
 			if (err.statusCode == 404) {
-				console.log("tweet "+id+" is NOT good, and should be deleted...");
-				//deleteRecord(id);
+				console.log("tweet "+id+" is NOT good, and should be hidden...");
+				callBack();
 			} else {
 				console.log("tweet "+id+" caused an error from Twitter.");
 			}
-			return false;
 		} else {
-			//console.log("tweet "+id+" is good.");
+			// nothing
 		}
 	});
 }
 
-function deleteRecord(tweetID)
+function hideRecord(id)
 {
-	
-	var where = "Tweet_ID = "+tweetID;
-	console.log(where);
-
 	var postData = {
-		where: where,
-		f:"pjson"
+		f:"pjson",
+		token: TOKEN,
+		features: JSON.stringify([{attributes:{Hide: '1', FID: id}}]),
+		rollbackOnFailure:false
 	}; 
-	
 	postData = querystring.stringify(postData);
-	
 	var options = {
 		host: "services.arcgis.com",
 		method: "POST",
 		port: 80,
-		path: FEATURE_SERVICE+"/deleteFeatures",
+		path: FEATURE_SERVICE+"/updateFeatures",
 		headers:{"Content-Type": "application/x-www-form-urlencoded","Content-Length": postData.length}
 	}
-	
 	var req = http.request(options, function(res) {
 	  res.setEncoding('utf8');
 	  res.on('data', function (chunk) {
 		console.log('BODY: ' + chunk);
 	  });
 	});
-	
 	req.on('error', function(e) {
 	  console.log('problem with request: ' + e.message);
 	});
-	
+	console.log(postData);
 	req.write(postData);
 	req.end();
-	
 }
-
 
 driver();
